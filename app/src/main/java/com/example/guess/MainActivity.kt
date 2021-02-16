607072700817ecace066697c725089eed0bb41c5
gahwa17 <s24527109@gmail.com>
2021-02-17 01:12:13 +0800

    chinese langauage

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val secretNumber = SecretNumber()
-
+    val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,24 +20,24 @@ class MainActivity : AppCompatActivity() {
         //n:user guess num
         val n = number.text.toString().toInt()
         println("number: $n")
-        Log.d("MainActivity","number" + n)
+        Log.d(TAG,"number" + n)
 
         //Judge whether the number(n) is bigger / smaller / correct to the secret?
         val diff = secretNumber.validate(n)
-        var msg = "Bingo!"
+        var msg = getString(R.string.bingo)
         if(diff < 0){
-            msg = "Bigger!"
+            msg = getString(R.string.bigger)
         }
         else if(diff > 0){
-            msg = "Smaller!"
+            msg = getString(R.string.smaller)
         }
 //        Toast is not clearly seen
 //        Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
 
         AlertDialog.Builder(this)
-            .setTitle("Message")
+            .setTitle(getString(R.string.dialog_title))
             .setMessage(msg)
-            .setPositiveButton("OK",null)
+            .setPositiveButton(getString(R.string.OK),null)
             .show()
     }
 }
